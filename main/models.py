b43516767ba1c1ac2bebafea7f602d5b3e0f589e
showtime333 <sagadeev.artem2001@gmail.com>
2020-07-09 12:09:34 +0300

    Add Test and Question models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Test(models.Model):
+    test_title = models.CharField('название теста', max_length = 100)
+    pub_date = models.DateTimeField('дата публикации')
+
+    def __str__ (self):
+        return self.test_title
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
+
+class Question(models.Model):
+    test = models.ForeignKey(Test, on_delete = models.CASCADE)
+    with_options = models.BooleanField('тип вопроса')
+    text = models.TextField('текст вопроса')
+    image = models.ImageField('картинка')
+    options = models.TextField('варианты ответа')
+    correct_answers = models.TextField('правильные ответы')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
